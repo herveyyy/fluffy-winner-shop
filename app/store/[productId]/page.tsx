7f18123339b0d2d577ae2369ec5a9e3ef17a3215
herveyyy <captainhervey@gmail.com>
2025-08-27 21:57:26 +0800

    [fix] added promise to product pprofile

--- app/store/[productId]/page.tsx
+++ app/store/[productId]/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
-
 import { Button } from "@/components/ui/button";
-import { FaCartPlus, FaStore } from "react-icons/fa";
-import { FaBasketShopping, FaCartShopping } from "react-icons/fa6";
-type Props = {
-    params: { productId: string };
-};
-const ProductProfilePage = (props: Props) => {
+import { FaStore } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ productId: string }>;
+}) {
+    const { productId } = await params;
+
     return (
         <main className="mt-12 max-w-6xl mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 gap-12">
             {/* Product Image */}
@@ -24,40 +27,14 @@ const ProductProfilePage = (props: Props) => {
             {/* Product Details */}
             <div className="flex flex-col justify-center">
                 <h1 className="text-4xl font-extrabold uppercase mb-4">
-                    {"Product Title"}
+                    Product ID: {productId}
                 </h1>
                 <p className="text-xl font-bold mb-6">{`Product Price`}</p>
                 <p className="text-lg mb-6">
                     Voluptate excepteur fugiat adipisicing anim occaecat
                     voluptate cillum eiusmod Lorem aliquip.
                 </p>
-                {/* Sizes */}
-                {/* <div className="mb-6">
-                    <h3 className="font-bold mb-2">Sizes</h3>
-                    <div className="flex gap-3">
-                        {product.sizes.map((size) => (
-                            <button
-                                key={size}
-                                className="px-4 py-2 border-2 border-black rounded-lg hover:bg-yellow-200"
-                            >
-                                {size}
-                            </button>
-                        ))}
-                    </div>
-                </div> */}
-                {/* Colors */}
-                <div className="mb-6">
-                    {/* <h3 className="font-bold mb-2">Colors</h3> */}
-                    {/* <div className="flex gap-3">
-                        {product.colors.map((color) => (
-                            <div
-                                key={color}
-                                className="w-10 h-10 rounded-full border-2 border-black cursor-pointer"
-                                style={{ backgroundColor: color.toLowerCase() }}
-                            />
-                        ))}
-                    </div> */}
-                </div>{" "}
+                <div className="mb-6"></div>
                 <Button
                     color="pink"
                     className="w-full  mt-6 text-lg py-6 font-bold rounded-2xl shadow-[6px_6px_0px_rgba(0,0,0,1)]"
@@ -66,10 +43,8 @@ const ProductProfilePage = (props: Props) => {
                 </Button>
                 <Button className="w-full mt-6 text-lg py-6 font-bold rounded-2xl shadow-[6px_6px_0px_rgba(0,0,0,1)]">
                     <FaCartShopping /> <span> Add to Cart</span>
-                </Button>{" "}
+                </Button>
             </div>
         </main>
     );
-};
-
-export default ProductProfilePage;
+}
